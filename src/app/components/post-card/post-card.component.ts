682bb4ac339221b5d9919b290162ba5582a828c7
Mike Hartington <mikehartington@gmail.com>
2023-06-04 13:49:11 -0400

    updates

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -11,9 +11,7 @@ import {
 } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { BskService } from 'src/app/services/bsk/bsk.service';
-import {
-  PostView,
-} from '@atproto/api/dist/client/types/app/bsky/feed/defs';
+import { PostView } from '@atproto/api/dist/client/types/app/bsky/feed/defs';
 import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'post-card',
@@ -30,6 +28,9 @@ export class PostCardComponent {
     this._thread.set(_thread);
   }
 
+  @Input() set reason(_reason: any) {
+    this._reason.set(_reason);
+  }
 
   private bsk = inject(BskService);
   private rtr = inject(Router);
@@ -37,17 +38,21 @@ export class PostCardComponent {
 
   readonly _post = signal<PostView | null>(null);
   readonly _thread = signal<any>(null);
+  readonly _reason = signal<any>(null);
 
   readonly record = computed<any>(() => this._post()?.record);
   readonly embeds = computed<any>(() => this._post()?.embed);
 
-  @Output() onPostLoad = new EventEmitter()
+  readonly viewReason = signal('');
+  @Output() onPostLoad = new EventEmitter();
 
-  readonly el = inject(ElementRef)
-  ngAfterViewInit(){
+  @Input() link: string = '';
+
+  readonly el = inject(ElementRef);
+  ngAfterViewInit() {
     setTimeout(() => {
-    this.onPostLoad.emit({target: this.el.nativeElement});
-    }, 16)
+      this.onPostLoad.emit({ target: this.el.nativeElement });
+    }, 16);
   }
   async toggleLike(e: MouseEvent) {
     e.stopPropagation();
@@ -59,7 +64,11 @@ export class PostCardComponent {
     await this.updatePost();
   }
   reply() {}
-  repost() {}
+  async repost(e: MouseEvent) {
+    e.stopPropagation();
+    await this.bsk.agent?.repost(this._post()?.uri!, this._post()?.cid!);
+    await this.updatePost();
+  }
   async updatePost() {
     const res = await this.bsk.agent?.getPostThread({
       uri: this._post()?.uri!,
@@ -70,5 +79,32 @@ export class PostCardComponent {
     e.stopPropagation();
     const root = this.route.snapshot.url[0].path;
     this.rtr.navigate(['app', root, this._post()?.author.handle]);
+  }
+
+  timeSince(date: string) {
+    const dateFromAPI = new Date(date) as any;
+    var seconds = Math.floor(((new Date() as any) - dateFromAPI) / 1000);
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + 'y';
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + 'mo';
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + 'd';
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + 'h';
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + 'm';
+    }
+    return Math.floor(seconds) + 's';
   }
 }
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,31 +2,43 @@ import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { BskService } from 'src/app/services/bsk/bsk.service';
-import { PostCardComponent } from "../../components/post-card/post-card.component";
+import { PostCardComponent } from '../../components/post-card/post-card.component';
 import { RouterLink } from '@angular/router';
-import { ComposeModalComponent } from "../../components/compose-modal/compose-modal.component";
+import { ComposeModalComponent } from '../../components/compose-modal/compose-modal.component';
 import { ProfileViewDetailed } from '@atproto/api/dist/client/types/app/bsky/actor/defs';
 
 @Component({
-    selector: 'app-tab1',
-    templateUrl: 'tab1.page.html',
-    styleUrls: ['tab1.page.scss'],
-    standalone: true,
-    imports: [IonicModule, JsonPipe, NgFor, NgIf, PostCardComponent, RouterLink, ComposeModalComponent]
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    JsonPipe,
+    NgFor,
+    NgIf,
+    PostCardComponent,
+    RouterLink,
+    ComposeModalComponent,
+  ],
 })
 export class Tab1Page {
   public feed = signal<any[]>([]);
-  private bsk = inject(BskService)
-  public actor = signal<ProfileViewDetailed | undefined>(undefined)
+  private bsk = inject(BskService);
+  public actor = signal<ProfileViewDetailed | undefined>(undefined);
   constructor() {}
-  async ngOnInit(){
-    const res = await this.bsk.getTimeLine();
-    if(res){
-      this.feed.set(res);
+  async ionViewDidEnter() {
+    if (this.feed().length === 0) {
+      const res = await this.bsk.getTimeLine();
+      if (res) {
+        this.feed.set(res);
+      }
     }
 
-    const did = await this.bsk.agent?.resolveHandle({handle: this.bsk.handle!}).then(res => res.data.did)!
-    const actorData = await this.bsk.agent?.getProfile({actor: did})
-    this.actor.set(actorData?.data)
+    const did = await this.bsk.agent
+      ?.resolveHandle({ handle: this.bsk.handle! })
+      .then((res) => res.data.did)!;
+    const actorData = await this.bsk.agent?.getProfile({ actor: did });
+    this.actor.set(actorData?.data);
   }
 }
